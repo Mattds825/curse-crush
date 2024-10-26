be2f5267b0a5676af4c26e45753811d91b6b7b16
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-10-26 16:19:36 +0100

    add function to create 8x8 board on load and display it as images with colored backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-function App() {
+import React, { useEffect, useState } from "react";
+const width: number = 8;
+const tileColors: string[] = [
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "yellow",
+];
 
-  return (
-    <>
-      
-    </>
-  )
-}
+const App = () => {
 
-export default App
+  const [board, setBoard] = useState<string[]>([]);
+
+  const createBoard = () => {
+    const board: string[] = [];
+    for (let i = 0; i < width * width; i++) {
+      // get a random color from the tileColors array
+      const randomColor: string = tileColors[Math.floor(Math.random() * tileColors.length)];
+      board.push(randomColor);
+    }
+    setBoard(board);
+  };
+
+  // create the board when the component mounts
+  useEffect(() => {
+    createBoard();
+  },[])
+
+  return <div className="app">
+    <div className="game">
+      {board.map((color: string, index: number) => {
+        return <img key={index} alt={color} style={{backgroundColor: color}}/>
+      })}
+    </div>
+  </div>;
+};
+
+export default App;
